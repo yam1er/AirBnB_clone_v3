ccc78132e7d5c1704a60509eb88b5e32ab65b866
Alphonse <alphonsemehounme9gmail.com>
2024-06-10 17:04:00 -0700

    Fix pycodestyle error on app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,13 +7,16 @@ from flask import Flask
 from os import getenv
 from models import storage
 
+
 app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
+
 @app.teardown_appcontext
 def close_db(exception):
     storage.close()
+
 
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
