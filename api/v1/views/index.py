dba2f4fa57dce5865c9ce2b4de303bfc95caf1e5
Alphonse <alphonsemehounme9gmail.com>
2024-06-11 03:58:02 -0700

    Implement stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Create Flask app views
 '''
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -12,4 +13,18 @@ def status():
         call status
     """
     response = {'status': "OK"}
+    return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stat():
+    """Give class stats"""
+    response = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
     return jsonify(response)
